2e0d12eec86bedff15c2cf040a113daeea718d69
Gabor Csardi <csardi.gabor@gmail.com>
2014-09-06 20:06:36 -0400

    Add gtr and ltr

--- R/func-range.r
+++ R/func-range.r
@@ -39,13 +39,79 @@ valid_range <- function(range, loose = FALSE) {
 #' @export
 
 ltr <- function(version, range, loose = FALSE) {
-  TODO
+  outside(version, range, "<", loose)
 }
 
 #' @export
 
 gtr <- function(version, range, loose = FALSE) {
-  TODO
+  outside(version, range, ">", loose)
+}
+
+outside <- function(version, range, hilo, loose) {
+
+  version <- semver$new(version, loose)
+  range <- semver::range$new(range, loose)
+
+  if (hilo == ">") {
+    gt_fn <- gt
+    lte_fn <- lte
+    lt_fn <- lt
+    comp <- ">"
+    ecomp <- ">="
+
+  } else if (hilo == "<") {
+    gt_fn <- lt
+    lte_fn <- gte
+    lt_fn <- gt
+    comp <- "<"
+    ecomp <- "<="
+
+  } else {
+    stop("Internal error, this should not happen")
+  }
+
+  ## If it satisifes the range it is not outside
+  if (satisfies(version, range, loose)) {
+    return(FALSE)
+  }
+
+  ## From now on, variable terms are as if we're in "gtr" mode.
+  ## but note that everything is flipped for the "ltr" function.
+
+  for (comparators in range$set) {
+
+    high <- NULL
+    low <- NULL
+
+    for (comparator in comparators) {
+      high <- high %||% comparator
+      low <- low %||% comparator
+      if (gt_fn(comparator$semver, high$semver, loose)) {
+        high <- comparator
+      } else if (lt_fn(comparator$semver, low$semver, loose)) {
+        low <- comparator
+      }
+    }
+
+    ## If the edge version comparator has a operator then our version
+    ## isn't outside it
+    if ((high$operator %===% comp) %||% (high$operator %===% ecomp)) {
+      return(FALSE)
+    }
+
+    ## If the lowest version comparator has an operator and our version
+    ## is less than it then it isn't higher than the range
+    if ((nay(low$operator) %||% (low$operator %===% comp)) &&
+        lte_fn(version, low$semver)) {
+      return(FALSE)
+    } else if ((low$operator %===% ecomp) &&
+               (lt_fn(version, low$semver))) {
+      return(FALSE)
+    }
+  }
+
+  TRUE
 }
 
 to_comparators <- function(range, loose = FALSE) {
